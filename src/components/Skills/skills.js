2ad35a263415d55ceb0444e8dedc9917e81020a8
Tushar Khurana <tusharkhurana526@gmail.com>
2024-06-07 13:26:32 +0530

    changed Progress bar

--- src/components/Skills/skills.js
+++ src/components/Skills/skills.js
@@ -1,100 +1,57 @@
 import React from 'react';
-import './Skills.css'; // Ensure this CSS file includes the styles for SkillBar and Skill-Area
-import skills from './Skills.svg';
-const Skills = () => (
-  <><h1>Skills</h1>
-    <div className = 'Issue'>
-      
-      <div>
-          <img src = {skills} alt = "skills" className = "skills" style={{height: '400px', width: '400px' }} />
-      </div>
-    <div>
-    
-    
-    <div>
-    <div className="SkillBar">
-      <div id="Skill-Cplus">
-        <a href="#education-section" className="SkillBar">
-          <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false" width="35" height="35" preserveAspectRatio="xMidYMid meet" viewBox="0 0 256 288" className="Skill-Area iconify" data-icon="logos:c-plusplus" data-inline="false" data-width="35" data-height="35" style={{ transform: 'rotate(360deg)' }}>
-            <path fill="#649AD2" d="M255.987 84.59c-.002-4.837-1.037-9.112-3.13-12.781c-2.054-3.608-5.133-6.632-9.261-9.023c-34.08-19.651-68.195-39.242-102.264-58.913c-9.185-5.303-18.09-5.11-27.208.27c-13.565 8-81.48 46.91-101.719 58.632C4.071 67.6.015 74.984.013 84.58C0 124.101.013 163.62 0 203.141c0 4.73.993 8.923 2.993 12.537c2.056 3.717 5.177 6.824 9.401 9.269c20.24 11.722 88.164 50.63 101.726 58.631c9.121 5.382 18.027 5.575 27.215.27c34.07-19.672 68.186-39.262 102.272-58.913c4.224-2.444 7.345-5.553 9.401-9.267c1.997-3.614 2.992-7.806 2.992-12.539c0 0 0-79.018-.013-118.539"></path>
-            <path fill="#004482" d="m128.392 143.476l-125.4 72.202c2.057 3.717 5.178 6.824 9.402 9.269c20.24 11.722 88.164 50.63 101.726 58.631c9.121 5.382 18.027 5.575 27.215.27c34.07-19.672 68.186-39.262 102.272-58.913c4.224-2.444 7.345-5.553 9.401-9.267z"></path>
-            <path fill="#1A4674" d="M91.25 164.863c7.297 12.738 21.014 21.33 36.75 21.33c15.833 0 29.628-8.7 36.888-21.576l-36.496-21.141z"></path>
-            <path fill="#01589C" d="M255.987 84.59c-.002-4.837-1.037-9.112-3.13-12.781l-124.465 71.667l124.616 72.192c1.997-3.614 2.99-7.806 2.992-12.539c0 0 0-79.018-.013-118.539"></path>
-            <path fill="#FFF" d="M249.135 148.636h-9.738v9.74h-9.74v-9.74h-9.737V138.9h9.737v-9.738h9.74v9.738h9.738zM128 58.847c31.135 0 58.358 16.74 73.17 41.709l.444.759l-37.001 21.307c-7.333-12.609-20.978-21.094-36.613-21.094c-23.38 0-42.333 18.953-42.333 42.332a42.13 42.13 0 0 0 5.583 21.003c7.297 12.738 21.014 21.33 36.75 21.33c15.659 0 29.325-8.51 36.647-21.153l.241-.423l36.947 21.406c-14.65 25.597-42.228 42.851-73.835 42.851c-31.549 0-59.084-17.185-73.754-42.707c-7"></path>
-          </svg>
-        </a>
-      </div>
-    </div>
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import './Skills.css';
+// import './Footer.css';
+// import { Icon } from '@iconify/react';
+// import cIcon from '@iconify/icons-logos/c';
+// import pythonIcon from '@iconify/icons-logos/python';
+// import html5Icon from '@iconify/icons-logos/html-5';
+// import css3Icon from '@iconify/icons-logos/css-3';
+// import javascriptIcon from '@iconify/icons-logos/javascript';
+// import skillImage from '../Assets/Skills.svg';
 
-    <div className="SkillBar">
-      <div id="Skill-C">
-        <a href="#education-section" className="SkillBar">
-          <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false" width="35" height="35" preserveAspectRatio="xMidYMid meet" viewBox="0 0 256 288" className="Skill-Area iconify" data-icon="logos:c" data-inline="false" data-width="35" data-height="35" style={{ transform: 'rotate(360deg)' }}>
-            <path fill="#A9B9CB" d="M255.987 85.672c-.002-4.843-1.037-9.122-3.129-12.794c-2.055-3.612-5.134-6.638-9.262-9.032c-34.081-19.67-68.195-39.28-102.264-58.97c-9.185-5.307-18.091-5.114-27.208.27c-13.565 8.008-81.481 46.956-101.719 58.689C4.071 68.665.015 76.056.013 85.663C0 125.221.013 164.777 0 204.336c.002 4.736.993 8.932 2.993 12.55c2.056 3.72 5.177 6.83 9.401 9.278c20.239 11.733 88.164 50.678 101.726 58.688c9.121 5.387 18.027 5.579 27.215.27c34.07-19.691 68.186-39.3 102.272-58.97c4.224-2.447 7.345-5.559 9.401-9.276c1.997-3.618 2.99-7.814 2.992-12.551c0 0 0-79.094-.013-118.653"></path>
-            <path fill="#7F8B99" d="M141.101 5.134c-9.17-5.294-18.061-5.101-27.163.269C100.395 13.39 32.59 52.237 12.385 63.94C4.064 68.757.015 76.129.013 85.711C0 125.166.013 164.62 0 204.076c.002 4.724.991 8.909 2.988 12.517c2.053 3.711 5.169 6.813 9.386 9.254a9008.51 9008.51 0 0 0 20.159 11.62L219.625 50.375c-26.178-15.074-52.363-30.136-78.524-45.241"></path>
-            <path fill="#FFF" d="m154.456 126.968l39.839.281c0-16.599-16.802-57.249-64.973-57.249c-30.691 0-71.951 19.512-71.951 75.61c0 56.097 40.447 74.39 71.951 74.39c51.017 0 63.21-35.302 63.21-55.252l-38.007-2.173s1.017 23.075-25.406 23.075c-24.39 0-28.46-29.878-28.46-40.04c0-15.447 5.493-40.244 28.46-40.244c22.968 0 25.337 21.602 25.337 21.602"></path>
-          </svg>
-        </a>
-      </div>
+const skills = () => {
+  return (
+    <div className="container-fluid body_section">
+        <section className="Section1">
+        <div className="row">
+          <div className="col-lg-6 col-md-6">
+            {/* <img className="skill-img" src={skillImage} alt="Skills" /> */}
+          </div>
+          <div className="col-lg-6 col-md-6">
+            <div id="SkillBox">
+              <center className="Skill-heading">Proficiency</center>
+              <div className="SkillBar">
+                <div id="Skill-C">
+                  {/* <Icon className="progress-icon" icon={cIcon} width="35" height="35" /> */}
+                </div>
+              </div>
+              <div className="SkillBar">
+                <div id="Skill-Python">
+                  {/* <Icon className="progress-icon" icon={pythonIcon} width="35" height="35" /> */}
+                </div>
+              </div>
+              <div className="SkillBar">
+                <div id="Skill-HTML">
+                  {/* <Icon className="progress-icon" icon={html5Icon} width="35" height="35" /> */}
+                </div>
+              </div>
+              <div className="SkillBar">
+                <div id="Skill-CSS">
+                  {/* <Icon className="progress-icon" icon={css3Icon} width="35" height="35" /> */}
+                </div>
+              </div>
+              <div className="SkillBar">
+                <div id="Skill-JS">
+                  {/* <Icon className="progress-icon" icon={javascriptIcon} width="35" height="35" /> */}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
-    <div className="SkillBar">
-  <div id="Skill-Python">
-    <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false" width="35" height="35" preserveAspectRatio="xMidYMid meet" viewBox="0 0 256 255" className="Skill-Area iconify" data-icon="logos:python" data-inline="false" data-width="35" data-height="35" style={{ transform: 'rotate(360deg)' }}>
-      <defs>
-        <linearGradient id="IconifyId-18fc73c80a0-4106ad-0" x1="12.959%" x2="79.639%" y1="12.039%" y2="78.201%">
-          <stop offset="0%" stop-color="#387EB8"></stop>
-          <stop offset="100%" stop-color="#366994"></stop>
-        </linearGradient>
-        <linearGradient id="IconifyId-18fc73c80a0-4106ad-1" x1="19.128%" x2="90.742%" y1="20.579%" y2="88.429%">
-          <stop offset="0%" stop-color="#FFE052"></stop>
-          <stop offset="100%" stop-color="#FFC331"></stop>
-        </linearGradient>
-      </defs>
-      <path fill="url(#IconifyId-18fc73c80a0-4106ad-0)" d="M126.916.072c-64.832 0-60.784 28.115-60.784 28.115l.072 29.128h61.868v8.745H41.631S.145 61.355.145 126.77c0 65.417 36.21 63.097 36.21 63.097h21.61v-30.356s-1.165-36.21 35.632-36.21h61.362s34.475.557 34.475-33.319V33.97S194.67.072 126.916.072M92.802 19.66a11.12 11.12 0 0 1 11.13 11.13a11.12 11.12 0 0 1-11.13 11.13a11.12 11.12 0 0 1-11.13-11.13a11.12 11.12 0 0 1 11.13-11.13"></path>
-      <path fill="url(#IconifyId-18fc73c80a0-4106ad-1)" d="M128.757 254.126c64.832 0 60.784-28.115 60.784-28.115l-.072-29.127H127.6v-8.745h86.441s41.486 4.705 41.486-60.712c0-65.416-36.21-63.096-36.21-63.096h-21.61v30.355s1.165 36.21-35.632 36.21h-61.362s-34.475-.557-34.475 33.32v56.013s-5.235 33.897 62.518 33.897m34.114-19.586a11.12 11.12 0 0 1-11.13-11.13a11.12 11.12 0 0 1 11.13-11.131a11.12 11.12 0 0 1 11.13 11.13a11.12 11.12 0 0 1-11.13 11.13"></path>
-    </svg>
-  </div>
-</div>
-<div className="SkillBar">
-  <div id="Skill-HTML">
-    <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false" width="35" height="35" preserveAspectRatio="xMidYMid meet" viewBox="0 0 256 361" className="Skill-Area iconify" data-icon="logos:html-5" data-inline="false" data-width="35" data-height="35" style={{ transform: 'rotate(360deg)' }}>
-      <path fill="#E44D26" d="m255.555 70.766l-23.241 260.36l-104.47 28.962l-104.182-28.922L.445 70.766z"></path>
-      <path fill="#F16529" d="m128 337.95l84.417-23.403l19.86-222.49H128z"></path>
-      <path fill="#EBEBEB" d="M82.82 155.932H128v-31.937H47.917l.764 8.568l7.85 88.01H128v-31.937H85.739zm7.198 80.61h-32.06l4.474 50.146l65.421 18.16l.147-.04V271.58l-.14.037l-35.568-9.604z"></path>
-      <path d="M24.18 0h16.23v16.035h14.847V0h16.231v48.558h-16.23v-16.26H40.411v16.26h-16.23V0zm68.65 16.103H78.544V0h44.814v16.103h-14.295v32.455h-16.23V16.103zM130.47 0h16.923l10.41 17.062L168.203 0h16.93v48.558h-16.164V24.49l-11.166 17.265h-.28L146.35 24.49v24.068h-15.88zm62.74 0h16.235v32.508h22.824v16.05h-39.06z"></path>
-      <path fill="#FFF" d="M127.89 220.573h39.327l-3.708 41.42l-35.62 9.614v33.226l65.473-18.145l.48-5.396l7.506-84.08l.764-8.568h-109.25l2.41 27.047H127.89zm0-63.707v.02h61.909l2.048-22.988l.215-2.41h-64.172v.02H82.82l2.414 27.047z"></path>
-    </svg>
-  </div>
-</div>
-<div className="SkillBar">
-  <div id="Skill-CSS">
-    <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false" width="35" height="35" preserveAspectRatio="xMidYMid meet" viewBox="0 0 256 361" className="Skill-Area iconify" data-icon="logos:css-3" data-inline="false" data-width="35" data-height="35" style={{ transform: 'rotate(360deg)' }}>
-      <path fill="#264DE4" d="m0 70.46l23.242 260.36l104.47 28.963l104.181-28.923L256 70.46H0z"></path>
-      <path fill="#2965F1" d="M128 337.95l84.417-23.403l19.86-222.49H128z"></path>
-      <path fill="#EBEBEB" d="M82.82 155.932H128v-31.937H47.917l.764 8.568l7.85 88.01H128v-31.937H85.739zm7.198 80.61h-32.06l4.474 50.146l65.421 18.16l.147-.04V271.58l-.14.037l-35.568-9.604z"></path>
-      <path fill="#FFF" d="M106.9 74.68v.02h64.062l-2.41 27.047h-61.652v.02h.02v31.937h57.793l-2.433 27.072H82.82l2.414 27.047h75.65l-7.006 78.505l-52.858 14.209l-.14.038l-52.9-14.221l-3.742-41.906H22.352h-.02l7.852 88.017l.147.04l97.669 27.213l.14-.04l97.652-27.214l.48-5.395l11.21-125.27l.764-8.568H109.29V77.493H106.9zm46.418 0H192v14.066h-22.505v2.813H192v32.352h-38.681V34.46h22.505v-2.813H153.32z"></path>
-    </svg>
-  </div>
-</div>
-<div className="SkillBar">
-  <div id="Skill-JS">
-    <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false" width="35" height="35" preserveAspectRatio="xMidYMid meet" viewBox="0 0 256 256" className="Skill-Area iconify" data-icon="logos:javascript" data-inline="false" data-width="35" data-height="35" style={{ transform: 'rotate(360deg)' }}>
-      <path fill="#F7DF1E" d="M0 0h256v256H0z"></path>
-      <path d="m67.312 213.932l19.59-11.856c3.78 6.701 7.218 12.371 15.465 12.371c7.905 0 12.89-3.092 12.89-15.12v-81.798h24.057v82.138c0 24.917-14.606 36.259-35.916 36.259c-19.245 0-30.416-9.967-36.087-21.996m85.07-2.576l19.588-11.341c5.157 8.421 11.859 14.607 23.715 14.607c9.969 0 16.325-4.984 16.325-11.858c0-8.248-6.53-11.17-17.528-15.98l-6.013-2.58c-17.357-7.387-28.87-16.667-28.87-36.257c0-18.044 13.747-31.792 35.228-31.792c15.294 0 26.292 5.328 34.196 19.247l-18.732 12.03c-4.125-7.389-8.591-10.31-15.465-10.31c-7.046 0-11.514 4.468-11.514 10.31c0 7.217 4.468 10.14 14.778 14.608l6.014 2.577c20.45 8.765 31.963 17.7 31.963 37.804c0 21.654-17.012 33.51-39.867 33.51c-22.339 0-36.774-10.654-43.819-24.574"></path>
-    </svg>
-  </div>
-</div>
-<div className="SkillBar">
-  <div id="Skill-SQL">
-    <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false" width="35" height="35" style={{ color: 'black', transform: 'rotate(360deg)' }} preserveAspectRatio="xMidYMid meet" viewBox="0 0 20 20" className="Skill-Area iconify" data-icon="dashicons:database" data-inline="false" data-width="35" data-height="35">
-      <path fill="currentColor" d="M10 6c3.9 0 7-.9 7-2s-3.1-2-7-2s-7 .9-7 2s3.1 2 7 2m0 9c-3.9 0-7-.9-7-2v3c0 1.1 3.1 2 7 2s7-.9 7-2v-3c0 1.1-3.1 2-7 2m0-4c-3.9 0-7-.9-7-2v3c0 1.1 3.1 2 7 2s7-.9 7-2V9c0 1.1-3.1 2-7 2m0-4c-3.9 0-7-.9-7-2v3c0 1.1 3.1 2 7 2s7-.9 7-2V5c0 1.1-3.1 2-7 2"></path>
-    </svg>
-  </div>
-</div>
-</div>
-  </div>
-  </div>
-  </>
-);
+  );
+}
 
-export default Skills;
+export default skills;
